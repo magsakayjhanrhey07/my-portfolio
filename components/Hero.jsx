c8767d125cec1d2704f0b65596617f298b6a68df
jhanrhey89 <jhanrey.magsakay1@gmail.com>
2026-07-20 20:08:46 +0800

    Update info

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -29,7 +29,7 @@ export default function Hero() {
               Email
             </a>
             <span className="flex items-center gap-1">+639364529721</span>
-            <a href="https://linkedin.com/in/yourname" className="flex items-center gap-1 hover:text-white">
+            <a href="https://www.linkedin.com/in/jhan-rhey-magsakay-300920416/" className="flex items-center gap-1 hover:text-white">
               LinkedIn
             </a>
             <a href="https://github.com/yourname" className="flex items-center gap-1 hover:text-white">
